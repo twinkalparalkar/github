16ef70c82176b7aa593f577e8d67f780b42a6f49
twinkalp <twinkalparalkar2@email.com>
2023-01-13 21:40:39 +0530

    second commit

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-console.log('I have git')
+const s=document.querySelector("#header-title");
+s.style.borderBottom="solid 2px black";
+const f=document.querySelector(".title")
+f.style.fontWeight="bold"
+f.style.color="green"
+console.log(f)
