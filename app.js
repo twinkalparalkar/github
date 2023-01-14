ddcc122ec107f55949598796c5ef726d502bec77
twinkalp <twinkalparalkar2@email.com>
2023-01-14 14:05:14 +0530

    3rd commit

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
+// Task 3
 const s=document.querySelector("#header-title");
 s.style.borderBottom="solid 2px black";
 const f=document.querySelector(".title")
 f.style.fontWeight="bold"
 f.style.color="green"
-console.log(f)
+
+// Task 4
+const ul=document.querySelector("#items");
+ul.children[2].style.background="green"
+
+for(let i=0;i<ul.children.length;i++){
+	ul.children[i].style.fontWeight="bold"
+}
