528cd13b7a5bb4fb18701e9ef40343646cd97097
twinkalp <twinkalparalkar2@email.com>
2023-01-15 23:08:43 +0530

    6th commit

--- app.js
+++ app.js
@@ -22,14 +22,47 @@
 // console.log(lc[0].children[0])
 
 // task 6
-const ul1=document.querySelector(".list-group")
-ul1.children[1].style.background="green"
-ul1.children[2].style.display = "none";
+// const ul1=document.querySelector(".list-group")
+// ul1.children[1].style.background="green"
+// ul1.children[2].style.display = "none";
 
-const ul=document.querySelectorAll(".list-group")
-ul[0].children[1].style.color="green"
-for(let i=0;i<ul[0].children.length;i+=2){
-	ul[0].children[i].style.background="green"
-	// console.log(i)
-}
+// const ul=document.querySelectorAll(".list-group")
+// ul[0].children[1].style.color="green"
+// for(let i=0;i<ul[0].children.length;i+=2){
+// 	ul[0].children[i].style.background="green"
+// 	// console.log(i)
+// }
+//Task 7
+const p=document.querySelector("#items")
+
+
+// p.parentNode.style.background="#ccc"
+p.parentElement.style.background="#ccc"
+console.log(p.children,p.lastElementChild,p.firstElementChild)
+p.firstElementChild.style.background="red"
+p.lastElementChild.style.background="red"
+console.log(p.children,p.firstChild,p.lastChild)
+
+console.log(p.nextSibling,p.previousSibling)
+console.log(p.nextElementSibling,p.previousElementSibling)
+p.nextElementSibling.style.background="pink"
+p.previousElementSibling.style.background="pink"
+var newDiv=document.createElement('div')
+newDiv.className='hello'
+newDiv.setAttribute('title','Hello div')
+console.log(newDiv)
+var newDivText=document.createTextNode('HEllo')
+newDiv.appendChild(newDivText)
+var c=document.querySelector('header .container')
+var h=document.querySelector('header h1')
+c.insertBefore(newDiv,h)
+// console.log(newDiv)
+var newDiv1=document.createElement('div')
+newDiv.className='hello'
+var newDivText1=document.createTextNode('HEllo')
+newDiv1.appendChild(newDivText1)
+var c1=document.querySelector('#main')
+var h1=document.querySelector('#items')
+c1.insertBefore(newDiv1,h1)
+
 
