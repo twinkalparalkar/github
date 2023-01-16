162fcd63caec1198d7c404023599626c97127918
twinkalp <twinkalparalkar2@email.com>
2023-01-16 14:25:24 +0530

    13th commit

--- simple.js
+++ simple.js
@@ -6,6 +6,23 @@ const ul=document.querySelector('#items')
 
 f.addEventListener('submit',additem)
 
+ul.addEventListener('click',removeitem)
+
+function removeitem(e){
+	e.preventDefault()
+	console.log(e.target.parentElement.innerText)
+	if(e.target.classList.contains('delete')){
+		var t=e.target.parentElement.innerText
+		const v=t.split(' - ')[1]
+		localStorage.removeItem(v)
+		console.log(t.split(' - '))
+		var l=e.target.parentElement;
+		ul.removeChild(l)
+	}
+	
+
+}
+
 function additem(e){
 	e.preventDefault()
 	if(n.value=="" || em.value=="" ||pho.value==""){
@@ -15,6 +32,10 @@ function additem(e){
 	}
 	const li=document.createElement('li')
 	li.appendChild(document.createTextNode(`${n.value} - ${em.value} - ${pho.value}`))
+	const d=document.createElement('button')
+	d.appendChild(document.createTextNode('Delete'))
+	d.className="delete"
+	li.appendChild(d)
 	ul.appendChild(li)
 	let m={
 		name:n.value,
@@ -23,6 +44,7 @@ function additem(e){
 	}
 	let m1=JSON.stringify(m)
 	localStorage.setItem(em.value,m1)
-	console.log(1)
+
+	// console.log(1)
 }
-// console.log(f)
+// localStorage.removeItem('amishaparalkar16@gmail.com')
