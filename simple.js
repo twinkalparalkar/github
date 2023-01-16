9992328a5604831cdee1e74a2424596e42048a9b
twinkalp <twinkalparalkar2@email.com>
2023-01-16 14:41:18 +0530

    14th commit

--- simple.js
+++ simple.js
@@ -10,7 +10,7 @@ ul.addEventListener('click',removeitem)
 
 function removeitem(e){
 	e.preventDefault()
-	console.log(e.target.parentElement.innerText)
+	// console.log(e.target.parentElement.innerText)
 	if(e.target.classList.contains('delete')){
 		var t=e.target.parentElement.innerText
 		const v=t.split(' - ')[1]
@@ -18,6 +18,19 @@ function removeitem(e){
 		console.log(t.split(' - '))
 		var l=e.target.parentElement;
 		ul.removeChild(l)
+	}
+	if(e.target.classList.contains('edit')){
+		console.log(112)
+		var t=e.target.parentElement.innerText
+		const v=t.split(' - ')
+		localStorage.removeItem(v[1])
+		console.log(v)
+		var l=e.target.parentElement;
+		ul.removeChild(l)
+		n.value=v[0]
+		em.value=v[1]
+		pho.value=v[2]
+
 	}
 	
 
@@ -31,11 +44,15 @@ function additem(e){
 		console.log(n.value)
 	}
 	const li=document.createElement('li')
-	li.appendChild(document.createTextNode(`${n.value} - ${em.value} - ${pho.value}`))
+	li.appendChild(document.createTextNode(`${n.value} - ${em.value} - ${pho.value} - `))
 	const d=document.createElement('button')
 	d.appendChild(document.createTextNode('Delete'))
 	d.className="delete"
 	li.appendChild(d)
+	const ed=document.createElement('button')
+	ed.appendChild(document.createTextNode('Edit'))
+	ed.className="edit"
+	li.appendChild(ed)
 	ul.appendChild(li)
 	let m={
 		name:n.value,
@@ -44,7 +61,10 @@ function additem(e){
 	}
 	let m1=JSON.stringify(m)
 	localStorage.setItem(em.value,m1)
-
+	n.value=''
+		em.value=''
+		pho.value=''
 	// console.log(1)
 }
-// localStorage.removeItem('amishaparalkar16@gmail.com')
+//1.click on edit..delete from localstorage and ul
+// popup into input box
