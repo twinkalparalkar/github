ef7fbf3a13e8d743d6a59695113dae01f50d8e60
twinkalp <twinkalparalkar2@email.com>
2023-03-03 23:50:57 +0530

    server commit2

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 const http=require('http')
 const server=http.createServer((req,res)=>{
-	console.log(req,"Twinkal Paralkar")
+	console.log(req.url,"Twinkal Paralkar")
+	res.setHeader('Content-Type','text/html')
+	res.write('<html>')
+	res.write('<head><title>My First Page</title></head>')
+	res.write('<body>')
+	if(req.url=='/home'){
+		res.write('<h1>Welcome home</h1>')}
+	else if(req.url=='/about'){
+		res.write('<h1>Welcome to about Us Page</h1>')}
+	else if(req.url=='/node'){
+		res.write('<h1>Welcome to my Node JS Project</h1>')}
+	res.write('</body>')
+	res.write('</html>')
+	res.end()
 })
 
 server.listen(4000)
