22256f3f0de8fbb4c8f53929f0ca4341d34e35c8
twinkalp <twinkalparalkar2@gmail.com>
2023-03-04 18:23:39 -0500

    server commit5

---
+++ routes.js
@@ -0,0 +1,34 @@
+const fs=require('fs')
+const requestHandler=((req,res)=>{
+	res.setHeader('Content-Type','text/html')
+	res.write('<html>')
+	res.write('<head><title>My First Page</title></head>')
+	res.write('<body>')
+    
+    	if(req.url=='/'){
+		res.write('<h1>Welcome home</h1>')
+        res.write('<form action="/" method="POST"><input type="text"name="message"><button type="submit">Send</button></form>')
+        
+        }
+        if(req.url=='/m'&& req.method=="POST"){
+            const body=[]
+            req.on('data',(chunk)=>{
+                console.log(chunk)
+                body.push(chunk)
+            })
+            return req.on('end',()=>{
+                const parsebody=Buffer.concat(body).toString()
+                const message=parsebody.split("=")[1]
+                fs.writeFileSync("m.txt",message)
+                res.statusCode=302
+                res.setHeader('Location','/')
+            })
+            
+            
+        }
+        res.setHeader('Content-Type','text/html')
+	res.write('</html>')
+	res.end()
+})
+
+module.exports=requestHandler
--- server.js
+++ server.js
@@ -1,22 +1,3 @@
-const http=require('http')
-const fs=require('fs')
-const server=http.createServer((req,res)=>{
-	res.setHeader('Content-Type','text/html')
-	res.write('<html>')
-	res.write('<head><title>My First Page</title></head>')
-	res.write('<body>')
-    res.write('<h1>Welcome home</h1>')
-    res.write('<form action="/" method="POST"><input type="text"name="message"><button type="submit">Send</button></form>')
-    fs.writeFileSync("m.txt",message)
-    res.statusCode=302
-    res.setHeader('Location','/')
-        res.setHeader('Location','/')
-	res.write('</html>')
-	res.end()
-})
-//http://localhost:4000/node
-server.listen(4000)
-
 // const http=require('http')
 // const fs=require('fs')
 // const server=http.createServer((req,res)=>{
@@ -24,31 +5,20 @@ server.listen(4000)
 // 	res.write('<html>')
 // 	res.write('<head><title>My First Page</title></head>')
 // 	res.write('<body>')
-    
-//     	if(req.url=='/'){
-// 		res.write('<h1>Welcome home</h1>')
-//         res.write('<form action="/" method="POST"><input type="text"name="message"><button type="submit">Send</button></form>')
-        
-//         }
-//         if(req.url=='/m'&& req.method=="POST"){
-//             const body=[]
-//             req.on('data',(chunk)=>{
-//                 console.log(chunk)
-//                 body.push(chunk)
-//             })
-//             return req.on('end',()=>{
-//                 const parsebody=Buffer.concat(body).toString()
-//                 const message=parsebody.split("=")[1]
-//                 fs.writeFileSync("m.txt",message)
-//                 res.statusCode=302
-//                 res.setHeader('Location','/')
-//             })
-            
-            
-//         }
-//         res.setHeader('Content-Type','text/html')
+//     res.write('<h1>Welcome home</h1>')
+//     res.write('<form action="/" method="POST"><input type="text"name="message"><button type="submit">Send</button></form>')
+//     fs.writeFileSync("m.txt",message)
+//     res.statusCode=302
+//     res.setHeader('Location','/')
+//         res.setHeader('Location','/')
 // 	res.write('</html>')
 // 	res.end()
 // })
 // //http://localhost:4000/node
 // server.listen(4000)
+
+const http=require('http')
+const routes=require('./routes')
+const server=http.createServer(routes)
+//http://localhost:4000/node
+server.listen(4000)
