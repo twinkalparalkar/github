64fb7612065c44b4c5162bb418101431a1291f6a
twinkalparalkar <41465430+twinkalparalkar@users.noreply.github.com>
2023-03-15 13:01:18 -0400

    server2

--- server.js
+++ server.js
@@ -1,19 +1,33 @@
-const http=require('http')
-const server=http.createServer((req,res)=>{
-	console.log(req.url,"Twinkal Paralkar")
-	res.setHeader('Content-Type','text/html')
-	res.write('<html>')
-	res.write('<head><title>My First Page</title></head>')
-	res.write('<body>')
-	if(req.url=='/home'){
-		res.write('<h1>Welcome home</h1>')}
-	else if(req.url=='/about'){
-		res.write('<h1>Welcome to about Us Page</h1>')}
-	else if(req.url=='/node'){
-		res.write('<h1>Welcome to my Node JS Project</h1>')}
-	res.write('</body>')
-	res.write('</html>')
-	res.end()
-})
+// const http=require('http')
+// const fs=require('fs')
+// const server=http.createServer((req,res)=>{
+// 	res.setHeader('Content-Type','text/html')
+// 	res.write('<html>')
+// 	res.write('<head><title>My First Page</title></head>')
+// 	res.write('<body>')
+//     res.write('<h1>Welcome home</h1>')
+//     res.write('<form action="/" method="POST"><input type="text"name="message"><button type="submit">Send</button></form>')
+//     fs.writeFileSync("m.txt",message)
+//     res.statusCode=302
+//     res.setHeader('Location','/')
+//         res.setHeader('Location','/')
+// 	res.write('</html>')
+// 	res.end()
+// })
+// //http://localhost:4000/node
+// server.listen(4000)
 
+const http=require('http')
+const express=require('express')
+const app=express()
+app.use((req,res,next)=>{
+    console.log("In the middle ware")
+    next()
+})
+app.use((req,res,next)=>{
+    console.log("In the middle ware")
+    res.send("<h1>hello world</h1>")
+})
+const server=http.createServer(app)
+//http://localhost:4000/node
 server.listen(4000)
