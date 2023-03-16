e0935e6a377ced47e7a0f450e378c1f540088cb7
twinkalparalkar <41465430+twinkalparalkar@users.noreply.github.com>
2023-03-15 22:54:38 -0400

    server3

--- server.js
+++ server.js
@@ -18,15 +18,23 @@
 // server.listen(4000)
 
 const http=require('http')
+const bo=require('body-parser')
 const express=require('express')
 const app=express()
-app.use((req,res,next)=>{
+app.use(bo.urlencoded({extended:false}))
+app.use('/add-product',(req,res,next)=>{
     console.log("In the middle ware")
-    next()
+    // res.send("<h1>Add Product</h1>")
+    res.send("<form action='/product' method='POST' ><input type='text' name='tit'><button type='submit'>ADD Product</button></form>")
 })
-app.use((req,res,next)=>{
+
+app.use('/product',(req,res)=>{
+    console.log(req.body)
+    res.redirect('/')
+})
+app.use('/',(req,res,next)=>{
     console.log("In the middle ware")
-    res.send("<h1>hello world</h1>")
+    res.send("<h1>hello world twinkal</h1>")
 })
 const server=http.createServer(app)
 //http://localhost:4000/node
